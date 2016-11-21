101b1024bcee3e88895803c3dba8013bdee46795
heoyunjee <heoyunjee@gmail.com>
2016-11-21 17:38:50 +0900

    Add router.js

---
+++ firstnodeserver/router.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+
+
+function home(request, response) {
+  if(request.url === '/') {
+    fs.readFile('./layout/home.html', function(err, data) {
+      response.write(data);
+      response.end();
+    });
+  }
+}
+
+function about(request, response) {
+  if(request.url === '/about') {
+    fs.readFile('./layout/about.html', function(err, data) {
+      response.write(data);
+      response.end();
+    });
+  }
+}
+
+function fastcampus(request, response) {
+  if(request.url === '/fastcampus') {
+    fs.readFile('./layout/fastcampus.html', function(err, data) {
+      response.write(data);
+      response.end();
+    });
+  }
+}
+
+function detail(request, response) {
+  // var detailId = request.url.replace("/", "");
+  //
+  // if(detailId.length > 0) {
+  //   fs.readFile('./layout/' + detailId + '.html', function(err, data) {
+  //     response.write(data);
+  //     response.end();
+  //   });
+  // }
+}
+
+module.exports.home = home;
+module.exports.about = about;
+module.exports.fastcampus = fastcampus;
+module.exports.detail = detail;
